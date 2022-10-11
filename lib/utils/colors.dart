3c4942e1c09860d4e684be44e19be59ee930b63f
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-12 00:23:26 +0200

    App Colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Black = const Color(0xff191919);
+  static const White = const Color(0xff6e6e6e);
+  static const Yellow = const Color(0xfffbc301);
+}
