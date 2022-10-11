e42890bb67519da5d0bc7b52629e27049fe9952d
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-12 00:22:56 +0200

    Text Widget

---
+++ lib/AppWidgets/AppText.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppText extends StatelessWidget {
+  final String text;
+  final Color color;
+  final double size;
+
+  AppText({
+    required this.text,
+    this.color = const Color(0xfffbc301),
+    this.size = 16,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        color: color,
+        fontSize: size,
+      ),
+    );
+  }
+}
