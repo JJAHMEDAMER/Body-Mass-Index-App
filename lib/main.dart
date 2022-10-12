ffc4d03b4d1f83d155e91746e35b5984759a9885
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-12 09:29:06 +0200

    Page content

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:bmi_calc_app/AppWidgets/AppText.dart';
+import 'package:bmi_calc_app/AppWidgets/sideStrips.dart';
 import 'package:bmi_calc_app/utils/colors.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,53 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: AppColors.Black,
-        body: AppText(text: "hello App",),
+        body: Column(
+          children: [
+            Container(
+              child: Column(
+                children: [
+                  AppText(
+                    text: "hello App",
+                    size: 12,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      AppText(
+                        text: "Height",
+                      ),
+                      AppText(
+                        text: "Height",
+                      )
+                    ],
+                  ),
+                  AppText(text: "Calculate"),
+                  AppText(text: "0"),
+                  AppText(text: "Normal Weight"),
+                ],
+              ),
+            ),
+            Container(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Strips(
+                  borderRadius: BorderRadius.horizontal(
+                    left: Radius.circular(100),
+                  ),
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                ),
+                SizedBox(height: 10),
+                Strips(
+                  borderRadius: BorderRadius.horizontal(
+                    right: Radius.circular(100),
+                  ),
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                ),
+              ],
+            )),
+          ],
+        ),
       ),
     );
   }
