6413b98196a8abcac7538e740889cef361cd4359
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-12 14:44:53 +0200

    new colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const Black = const Color(0xff191919);
-  static const White = const Color(0xff6e6e6e);
+  static const Grey = const Color(0xff6e6e6e);
+  static const White = Color.fromARGB(255, 219, 219, 219);
   static const Yellow = const Color(0xfffbc301);
 }
