cd1ae7782e191e493afa062e023a07c64343a8f4
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-10-12 16:55:01 +0200

    Add colors

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors{
   static const Black = const Color(0xff191919);
   static const Grey = const Color(0xff6e6e6e);
-  static const White = Color.fromARGB(255, 219, 219, 219);
+  static const White = Color.fromARGB(255, 242, 242, 242);
   static const Yellow = const Color(0xfffbc301);
+  static const DarkYellow = Color.fromARGB(255, 235, 184, 0);
 }
